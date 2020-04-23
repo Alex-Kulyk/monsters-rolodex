e6bd4a65afd42f98f4f2aefc661346a5ced2e3d8
Villiar <alexkulyk87@gmail.com>
2020-04-23 19:51:09 +0300

    Updates

---
+++ precache-manifest.009979d79efc68e587ed427b8078407a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d00218127682757e5bdf2f9b2ddab8d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "520ae51ae75fa2378dcf",
+    "url": "/monsters-rolodex/static/css/main.99f227f2.chunk.css"
+  },
+  {
+    "revision": "f15a965c5d6a755c5335",
+    "url": "/monsters-rolodex/static/js/2.0abfe508.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.0abfe508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "520ae51ae75fa2378dcf",
+    "url": "/monsters-rolodex/static/js/main.4fa0d70c.chunk.js"
+  },
+  {
+    "revision": "ac7692f361dea471e0d6",
+    "url": "/monsters-rolodex/static/js/runtime-main.ab61569e.js"
+  }
+]);
